b5eaede7fefc1ffc702e72c44551fa214567d75d
Uku Taht <uku.taht@gmail.com>
2016-04-24 19:17:15 +0100

    Reformat vm.c source file

--- vm/src/vm.c
+++ vm/src/vm.c
@@ -7,30 +7,30 @@
 #include "opcodes.h"
 
 vm_t *vm_new() {
-    vm_t *vm;
+  vm_t *vm;
 
-    vm = malloc(sizeof(struct vm));
-    if (!vm)
-        return NULL;
-    memset(vm, '\0', sizeof(struct vm));
+  vm = malloc(sizeof(struct vm));
+  if (!vm)
+    return NULL;
+  memset(vm, '\0', sizeof(struct vm));
 
-    /**
-     * Allocate 64k for the program.
-     */
-    vm->code = malloc(0xFFFF);
-    if (vm->code == NULL) {
-        return NULL;
-    }
+  /**
+   * Allocate 64k for the program.
+   */
+  vm->code = malloc(0xFFFF);
+  if (vm->code == NULL) {
+    return NULL;
+  }
 
-    vm->ip = 0;
-    vm->current_frame = 0;
-    vm->running = true;
+  vm->ip = 0;
+  vm->current_frame = 0;
+  vm->running = true;
 
-    memset(vm->code, '\0', 0xFFFF);
+  memset(vm->code, '\0', 0xFFFF);
 
-    opcode_init(vm);
+  opcode_init(vm);
 
-    return vm;
+  return vm;
 }
 
 
@@ -39,33 +39,33 @@ vm_t *vm_new() {
  * of modules separately.
  */
 void vm_load_module(vm_t *vm, unsigned char *code, unsigned int size) {
-    memcpy(vm->code, code, size);
+  memcpy(vm->code, code, size);
 }
 
 void vm_run(vm_t * vm) {
-    GC_init();
-    int iterations = 0;
+  GC_init();
+  int iterations = 0;
 
+  /**
+   * Run continuously.
+   *
+   * In practice this means until an EXIT instruction is encountered,
+   * which will set the "running"-flag to be false.
+   *
+   */
+  while (vm->running == true) {
     /**
-     * Run continuously.
-     *
-     * In practice this means until an EXIT instruction is encountered,
-     * which will set the "running"-flag to be false.
-     *
+     * Lookup the instruction at the instruction-pointer.
      */
-    while (vm->running == true) {
-        /**
-         * Lookup the instruction at the instruction-pointer.
-         */
-        int opcode = vm->code[vm->ip];
-        /**
-         * Call the opcode implementation, if defined.
-         */
-        if (vm->opcodes[opcode] != NULL)
-            vm->opcodes[opcode] (vm);
+    int opcode = vm->code[vm->ip];
+    /**
+     * Call the opcode implementation, if defined.
+     */
+    if (vm->opcodes[opcode] != NULL)
+      vm->opcodes[opcode] (vm);
 
-        iterations++;
-    }
+    iterations++;
+  }
 
-    debug_print("Executed %u instructions\n", iterations);
+  debug_print("Executed %u instructions\n", iterations);
 }
