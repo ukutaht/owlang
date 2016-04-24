421d0aa380f788860cf18f92a38304818061ff55
Uku Taht <uku.taht@gmail.com>
2016-04-24 04:18:49 +0100

    Define macros to deal with vector/rrb casting

--- vm/src/vector.c
+++ vm/src/vector.c
@@ -2,25 +2,25 @@
 #include "vector.h"
 #include "vm.h"
 
+#define rrb_to_vec(rrb) ((((owl_term) rrb) << 3) | VECTOR)
+#define vec_to_rrb(vec) ((RRB*) owl_extract_ptr(vec))
+
 owl_term vector_init() {
   const RRB *rrb = rrb_create();
-  owl_term vector = (owl_term) rrb;
-
-  return (vector << 3) | VECTOR;
+  return rrb_to_vec(rrb);
 }
 
 owl_term vector_push(owl_term vec, owl_term elem) {
-  const RRB *rrb = (RRB*) owl_extract_ptr(vec);
-  owl_term modified = (owl_term) rrb_push(rrb, (void*) elem);
-
-  return (modified << 3) | VECTOR;
+  const RRB *rrb = vec_to_rrb(vec);
+  rrb = rrb_push(rrb, (void*) elem);
+  return rrb_to_vec(rrb);
 }
 
 bool vector_eq(owl_term left_vec, owl_term right_vec) {
   if (left_vec == right_vec) return true;
 
-  const RRB *left = (RRB*) owl_extract_ptr(left_vec);
-  const RRB *right = (RRB*) owl_extract_ptr(right_vec);
+  const RRB *left = vec_to_rrb(left_vec);
+  const RRB *right = vec_to_rrb(right_vec);
 
   uint32_t left_count = rrb_count(left);
   uint32_t right_count = rrb_count(right);
