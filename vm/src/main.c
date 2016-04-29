458052d352ecef82aaaefaa87aa62a93a74aeed1
Uku Taht <uku.taht@gmail.com>
2016-04-29 16:52:15 +0100

    Format main.c

--- vm/src/main.c
+++ vm/src/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 
 #include "vm.h"
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Usage: %s input-file\n", argv[0]);
-        return 0;
-    }
-
-    vm_t *vm = vm_new();
-
-    vm_load_module_from_file(vm, argv[1]);
-
-    vm_run(vm);
-
+  if (argc < 2) {
+    printf("Usage: %s input-file\n", argv[0]);
     return 0;
+  }
+
+  vm_t *vm = vm_new();
+
+  vm_load_module_from_file(vm, argv[1]);
+
+  vm_run(vm);
+
+  return 0;
 }
